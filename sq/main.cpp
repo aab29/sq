00c4d701fc8601b69bc2e1090e46a4b5b14c69c2
Aaron Barrett <aaron@mindsnacks.com>
2014-10-01 18:32:56 -0700

    Initial Commit

---
+++ sq/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  sq
+//
+//  Created by Aaron Barrett on 10/1/14.
+//  Copyright (c) 2014 squirrel-lang. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
